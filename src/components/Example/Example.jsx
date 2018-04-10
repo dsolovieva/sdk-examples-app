0911d8fb9fdeac52c509d4667ec490cc43f0af2f
Daria Soloveva <Daria_Soloveva@epam.com>
2018-04-11 02:13:37 +0400

    try to fix URLs

--- src/components/Example/Example.jsx
+++ src/components/Example/Example.jsx
@@ -6,8 +6,8 @@ class Example extends Component {
   render() {
     return (
       <Fragment>
-        <ExampleResult src={`%PUBLIC_URL%/examples/${this.props.match.params.example || 'getting-started'}/index.html`} />
-        <ExampleSource url={`%PUBLIC_URL%/examples/${this.props.match.params.example || 'getting-started'}/code.html`} />
+        <ExampleResult src={`${process.env.PUBLIC_URL}/examples/${this.props.match.params.example || 'getting-started'}/index.html`} />
+        <ExampleSource url={`${process.env.PUBLIC_URL}/examples/${this.props.match.params.example || 'getting-started'}/code.html`} />
       </Fragment>
     );
   }
